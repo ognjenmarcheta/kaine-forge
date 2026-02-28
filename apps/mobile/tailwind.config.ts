ecd06c9652b79c2dda0d344800c44a189bf20c43
Ognjen Marčeta <ognjenmarcheta@gmail.com>
2026-02-28 21:35:02 +0100

    fix(mobile): use scoped import for tailwind preset

--- apps/mobile/tailwind.config.ts
+++ apps/mobile/tailwind.config.ts
@@ -1,6 +1,6 @@
+import preset from "@repo/config/tailwind/preset";
 import nativewindPreset from "nativewind/dist/tailwind/index.js";
 
-import preset from "../../packages/config/tailwind/preset.js";
 
 export default {
   content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
